c9352e5b51e859bc52a18224cbc371ff5e4a418e
Saif-Ul-llah <102151531+Saif-Ul-llah@users.noreply.github.com>
2022-09-13 13:13:03 +0500

    Add files via upload

---
+++ dom object/app.js
@@ -0,0 +1,477 @@
+var match = {
+    "name": "World Cup 2018",
+    "teams": [
+      {
+        "name": "Egypt",
+        "code": "EGY",
+        "continent": "Africa",
+        "assoc": {
+          "key": "egy",
+          "name": "Egyptian Football Association",
+          "continental": {
+            "name": "Confédération Africaine de Football (CAF)",
+            "code": "CAF"
+          }
+        }
+      },
+      {
+        "name": "Morocco",
+        "code": "MAR",
+        "continent": "Africa",
+        "assoc": {
+          "key": "mar",
+          "name": "Fédération Royale Marocaine de Football",
+          "continental": {
+            "name": "Confédération Africaine de Football (CAF)",
+            "code": "CAF"
+          }
+        }
+      },
+      {
+        "name": "Tunisia",
+        "code": "TUN",
+        "continent": "Africa",
+        "assoc": {
+          "key": "tun",
+          "name": "Fédération Tunisienne de Football",
+          "continental": {
+            "name": "Confédération Africaine de Football (CAF)",
+            "code": "CAF"
+          }
+        }
+      },
+      {
+        "name": "Senegal",
+        "code": "SEN",
+        "continent": "Africa",
+        "assoc": {
+          "key": "sen",
+          "name": "Fédération Sénégalaise de Football",
+          "continental": {
+            "name": "Confédération Africaine de Football (CAF)",
+            "code": "CAF"
+          }
+        }
+      },
+      {
+        "name": "Nigeria",
+        "code": "NGA",
+        "continent": "Africa",
+        "assoc": {
+          "key": "nga",
+          "name": "Nigeria Football Federation",
+          "continental": {
+            "name": "Confédération Africaine de Football (CAF)",
+            "code": "CAF"
+          }
+        }
+      },
+      {
+        "name": "Japan",
+        "code": "JPN",
+        "continent": "Asia",
+        "assoc": {
+          "key": "jpn",
+          "name": "Japan Football Association",
+          "continental": {
+            "name": "Asian Football Confederation (AFC)",
+            "code": "AFC"
+          }
+        }
+      },
+      {
+        "name": "South Korea",
+        "code": "KOR",
+        "continent": "Asia",
+        "assoc": {
+          "key": "kor",
+          "name": "Korea Football Association",
+          "continental": {
+            "name": "Asian Football Confederation (AFC)",
+            "code": "AFC"
+          }
+        }
+      },
+      {
+        "name": "Costa Rica",
+        "code": "CRC",
+        "continent": "Central America",
+        "assoc": {
+          "key": "crc",
+          "name": "Federación Costarricense de Fútbol",
+          "continental": {
+            "name": "Confederation of North, Central American and Caribbean Association Football (CONCACAF)",
+            "code": "CONCACAF"
+          }
+        }
+      },
+      {
+        "name": "Panama",
+        "code": "PAN",
+        "continent": "Central America",
+        "assoc": {
+          "key": "pan",
+          "name": "Federación Panameña de Fútbol",
+          "continental": {
+            "name": "Confederation of North, Central American and Caribbean Association Football (CONCACAF)",
+            "code": "CONCACAF"
+          }
+        }
+      },
+      {
+        "name": "Belgium",
+        "code": "BEL",
+        "continent": "Europe",
+        "assoc": {
+          "key": "bel",
+          "name": "Koninklijke Belgische Voetbalbond",
+          "continental": {
+            "name": "Union of European Football Associations (UEFA)",
+            "code": "UEFA"
+          }
+        }
+      },
+      {
+        "name": "Germany",
+        "code": "GER",
+        "continent": "Europe",
+        "assoc": {
+          "key": "ger",
+          "name": "Deutscher Fußball-Bund",
+          "continental": {
+            "name": "Union of European Football Associations (UEFA)",
+            "code": "UEFA"
+          }
+        }
+      },
+      {
+        "name": "Spain",
+        "code": "ESP",
+        "continent": "Europe",
+        "assoc": {
+          "key": "esp",
+          "name": "Real Federación Española de Fútbol",
+          "continental": {
+            "name": "Union of European Football Associations (UEFA)",
+            "code": "UEFA"
+          }
+        }
+      },
+      {
+        "name": "France",
+        "code": "FRA",
+        "continent": "Europe",
+        "assoc": {
+          "key": "fra",
+          "name": "Fédération Française de Football",
+          "continental": {
+            "name": "Union of European Football Associations (UEFA)",
+            "code": "UEFA"
+          }
+        }
+      },
+      {
+        "name": "Portugal",
+        "code": "POR",
+        "continent": "Europe",
+        "assoc": {
+          "key": "por",
+          "name": "Federação Portuguesa de Futebol",
+          "continental": {
+            "name": "Union of European Football Associations (UEFA)",
+            "code": "UEFA"
+          }
+        }
+      },
+      {
+        "name": "Denmark",
+        "code": "DEN",
+        "continent": "Europe",
+        "assoc": {
+          "key": "den",
+          "name": "Dansk Boldspil-Union",
+          "continental": {
+            "name": "Union of European Football Associations (UEFA)",
+            "code": "UEFA"
+          }
+        }
+      },
+      {
+        "name": "Poland",
+        "code": "POL",
+        "continent": "Europe",
+        "assoc": {
+          "key": "pol",
+          "name": "Polski Związek Piłki Nożnej",
+          "continental": {
+            "name": "Union of European Football Associations (UEFA)",
+            "code": "UEFA"
+          }
+        }
+      },
+      {
+        "name": "Sweden",
+        "code": "SWE",
+        "continent": "Europe",
+        "assoc": {
+          "key": "swe",
+          "name": "Svenska Fotbollförbundet",
+          "continental": {
+            "name": "Union of European Football Associations (UEFA)",
+            "code": "UEFA"
+          }
+        }
+      },
+      {
+        "name": "Switzerland",
+        "code": "SUI",
+        "continent": "Europe",
+        "assoc": {
+          "key": "sui",
+          "name": "Association Suisse de Football",
+          "continental": {
+            "name": "Union of European Football Associations (UEFA)",
+            "code": "UEFA"
+          }
+        }
+      },
+      {
+        "name": "Croatia",
+        "code": "CRO",
+        "continent": "Europe",
+        "assoc": {
+          "key": "cro",
+          "name": "Hrvatski Nogometni Savez",
+          "continental": {
+            "name": "Union of European Football Associations (UEFA)",
+            "code": "UEFA"
+          }
+        }
+      },
+      {
+        "name": "Serbia",
+        "code": "SRB",
+        "continent": "Europe",
+        "assoc": {
+          "key": "srb",
+          "name": "Fudbalski Savez Srbije",
+          "continental": {
+            "name": "Union of European Football Associations (UEFA)",
+            "code": "UEFA"
+          }
+        }
+      },
+      {
+        "name": "Russia",
+        "code": "RUS",
+        "continent": "Europe",
+        "assoc": {
+          "key": "rus",
+          "name": "Russian Football Union",
+          "continental": {
+            "name": "Union of European Football Associations (UEFA)",
+            "code": "UEFA"
+          }
+        }
+      },
+      {
+        "name": "Iceland",
+        "code": "ISL",
+        "continent": "Europe",
+        "assoc": {
+          "key": "isl",
+          "name": "Knattspyrnusamband Íslands",
+          "continental": {
+            "name": "Union of European Football Associations (UEFA)",
+            "code": "UEFA"
+          }
+        }
+      },
+      {
+        "name": "England",
+        "code": "ENG",
+        "continent": "Europe"
+      },
+      {
+        "name": "Iran",
+        "code": "IRN",
+        "continent": "Middle East",
+        "assoc": {
+          "key": "irn",
+          "name": "Football Federation Islamic Republic of Iran",
+          "continental": {
+            "name": "Asian Football Confederation (AFC)",
+            "code": "AFC"
+          }
+        }
+      },
+      {
+        "name": "Saudi Arabia",
+        "code": "KSA",
+        "continent": "Middle East",
+        "assoc": {
+          "key": "ksa",
+          "name": "Saudi Arabian Football Federation",
+          "continental": {
+            "name": "Asian Football Confederation (AFC)",
+            "code": "AFC"
+          }
+        }
+      },
+      {
+        "name": "Mexico",
+        "code": "MEX",
+        "continent": "North America",
+        "assoc": {
+          "key": "mex",
+          "name": "Federación Mexicana de Fútbol Asociación",
+          "continental": {
+            "name": "Confederation of North, Central American and Caribbean Association Football (CONCACAF)",
+            "code": "CONCACAF"
+          }
+        }
+      },
+      {
+        "name": "Australia",
+        "code": "AUS",
+        "continent": "Pacific",
+        "assoc": {
+          "key": "aus",
+          "name": "Football Federation Australia",
+          "continental": {
+            "name": "Asian Football Confederation (AFC)",
+            "code": "AFC"
+          }
+        }
+      },
+      {
+        "name": "Argentina",
+        "code": "ARG",
+        "continent": "South America",
+        "assoc": {
+          "key": "arg",
+          "name": "Asociación del Fútbol Argentino",
+          "continental": {
+            "name": "Confederación Sudamericana de Fútbol (CONMEBOL)",
+            "code": "CONMEBOL"
+          }
+        }
+      },
+      {
+        "name": "Brazil",
+        "code": "BRA",
+        "continent": "South America",
+        "assoc": {
+          "key": "bra",
+          "name": "Confederação Brasileira de Futebol",
+          "continental": {
+            "name": "Confederación Sudamericana de Fútbol (CONMEBOL)",
+            "code": "CONMEBOL"
+          }
+        }
+      },
+      {
+        "name": "Uruguay",
+        "code": "URU",
+        "continent": "South America",
+        "assoc": {
+          "key": "uru",
+          "name": "Asociación Uruguaya de Fútbol",
+          "continental": {
+            "name": "Confederación Sudamericana de Fútbol (CONMEBOL)",
+            "code": "CONMEBOL"
+          }
+        }
+      },
+      {
+        "name": "Colombia",
+        "code": "COL",
+        "continent": "South America",
+        "assoc": {
+          "key": "col",
+          "name": "Federación Colombiana de Fútbol",
+          "continental": {
+            "name": "Confederación Sudamericana de Fútbol (CONMEBOL)",
+            "code": "CONMEBOL"
+          }
+        }
+      },
+      {
+        "name": "Peru",
+        "code": "PER",
+        "continent": "South America",
+        "assoc": {
+          "key": "per",
+          "name": "Federación Peruana de Fútbol",
+          "continental": {
+            "name": "Confederación Sudamericana de Fútbol (CONMEBOL)",
+            "code": "CONMEBOL"
+          }
+        }
+      }
+    ]
+  }
+
+
+  var body = document.getElementById("body");
+  var h1 = document.createElement("h1");
+  h1.style.display="flex";
+  h1.style.justifyContent="center";
+  h1.style.alignItems ="center";
+
+  var h1_text = document.createTextNode(match.name);
+  h1.appendChild(h1_text);
+  body.appendChild(h1);
+
+  for (var key in match.teams){
+    var tea = match.teams[key];
+    var h3 = document.createElement("h3");
+    var h3_text = document.createTextNode(tea.name);
+    var teamsName = document.createElement("div");
+    teamsName.setAttribute("class","names");
+    
+    var inh = document.createElement("div")
+    var ul =document.createElement("ul")
+    var li1 = document.createElement("li");
+    var li2 = document.createElement("li");
+    var li3 = document.createElement("li");
+
+    var li1_text = document.createTextNode(tea.code)
+    var li2_text = document.createTextNode(tea.continent)
+    var li3_text = document.createTextNode(tea.assoc.name)
+
+
+    // var btn = document.createElement('button')
+    // btn.setAttribute("onClick", `but(${h3_text})`)
+    // btn.setAttribute("class","btn")
+    // var btnText = document.createTextNode('Details')
+    li1.appendChild(li1_text);
+    li2.appendChild(li2_text);
+    li3.appendChild(li3_text);
+    
+
+
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+
+    // inh.appendChild(ul);
+
+    teamsName.appendChild(h3_text);
+
+    teamsName.appendChild(ul)
+  //  teamsName.appendChild(inh)
+    // btn.appendChild(btnText);
+//    teamsName.appendChild(btn)
+    body.appendChild(teamsName);
+
+
+    
+
+// console.log( h3_text )
+
+  }
+  
+// function but(el) {
+//   console.log(el)
+// }
